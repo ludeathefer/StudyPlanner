8ad11dc343086ed5c71f6668afa8407b15c8c773
Regeneratel38 <44703575+Regeneratel38@users.noreply.github.com>
2023-08-07 22:59:28 +0545

    Update Assets.h

--- StudyPlanner/Assets.h
+++ StudyPlanner/Assets.h
@@ -8,4 +8,8 @@
 #define TITLEBAR_COLOUR wxColour("#403D4F")
 #define TITLEBAR_CLOSE_COLOUR wxColour("#B4293B")
 #define TITLEBAR_MINIMIZE_COLOUR wxColour("#683D77")
+#define CURRENT_DAY_COLOR wxColour("#B25022")
+#define SATURDAY_COLOR wxColour("#dd403a")
+#define SELECTED_DAY_COLOR wxColour("#a4303f")
+#define FOCUS_COLOR wxColour("#FF5E03"
 
